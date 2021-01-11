5200ecdc244c5bb40fa967db60b10d7e73b6993b
akeno himejima <alessio.carcavallo@08eins.swiss>
2021-01-11 09:52:05 +0100

    feat: setting up org.json

--- M411/src/main/java/ch/tbz/m411/Api.java
+++ M411/src/main/java/ch/tbz/m411/Api.java
@@ -1,9 +1,17 @@
 package ch.tbz.m411;
 
-public class Parser {
-    URL url = new URL("http://api.brewerydb.com/v2/styles/");
+import org.json.JSONObject;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
+/** Url object and type cast
+ * Main Class
+ */
+public class Api {
 
+    String apiKey= "?key=1511d0db4a1d6841481c672455358cff";
+    String apiUrl = "http://api.brewerydb.com/v2/";
 
 }
