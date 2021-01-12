001013492660339151ba6d9fcac2fd835e58a3fc
akeno himejima <alessio.carcavallo@08eins.swiss>
2021-01-12 08:25:12 +0100

    feat: method jsonToMap created and tested with strings

--- M411/src/main/java/ch/tbz/m411/api/Api.java
+++ M411/src/main/java/ch/tbz/m411/api/Api.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 /**
@@ -51,12 +54,29 @@ public class Api {
         }
     }
 
+    public static void jsonToMap(String t) throws JSONException {
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        JSONObject jObject = new JSONObject(t);
+        Iterator<?> keys = jObject.keys();
+
+        while( keys.hasNext() ){
+            String key = (String)keys.next();
+            String value = jObject.getString(key);
+            map.put(key, value);
+
+        }
+
+        System.out.println("json : "+jObject);
+        System.out.println("map : "+map);
+    }
+
     /**
      * Example of Call
      * @param args
      * @throws IOException
      * @throws JSONException
-
+    */
     public static void main(String[] args) throws IOException, JSONException {
         String apiUrl = "http://api.brewerydb.com/v2/beers";
         String apiKey = "?key=1511d0db4a1d6841481c672455358cff";
@@ -65,5 +85,6 @@ public class Api {
         System.out.println(apiUrl + apiKey + styleId);
         JSONObject json = readJsonFromUrl(apiUrl + apiKey + styleId);
         System.out.println(json.toString());
-    } */
+        jsonToMap("{\"test1\":\"1\",\"test2\":\"2\"}");
+    }
 }
