8f1b4f325ccf35ec3831a135e75df2eada76d158
antar <toobrokeforrysll@gmail.com>
2021-01-18 09:57:04 +0100

    feat: no static methods

--- M411/src/main/java/ch/tbz/m411/Main.java
+++ M411/src/main/java/ch/tbz/m411/Main.java
@@ -1,9 +1,12 @@
 package ch.tbz.m411;
 
+import ch.tbz.m411.api.BeerAdmin;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        BeerAdmin admin = new BeerAdmin();
+        admin.loadBeerStyles();
     }
 
 }
