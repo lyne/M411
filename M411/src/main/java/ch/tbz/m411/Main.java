37084ab0bfb4c6936a270c4bfd57d6330da7c963
antar <toobrokeforrysll@gmail.com>
2021-01-18 11:53:22 +0100

    feat: new methods for listing breweries

--- M411/src/main/java/ch/tbz/m411/Main.java
+++ M411/src/main/java/ch/tbz/m411/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         BeerAdmin admin = new BeerAdmin();
-        System.out.println("Loading beer styles...");
+        System.out.println("Loading beer styles and breweries...");
         admin.loadBeerStyles();
+        admin.getBreweries();
         Navigation navigation = new Navigation(admin);
         navigation.menu();
     }
